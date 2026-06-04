64dd36fa2d8c215a73fde28992b19209d32454fc
james-fsoft <57792460+james-fsoft@users.noreply.github.com>
2026-06-04 11:55:38 +0900

    Remove Supabase auth for public deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import MeetingApp from "./MeetingApp";
-import { createClient } from "@/lib/supabase-server";
 
-export default async function Home() {
-  const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
-  return <MeetingApp email={user?.email ?? ""} />;
+export default function Home() {
+  return <MeetingApp />;
 }
