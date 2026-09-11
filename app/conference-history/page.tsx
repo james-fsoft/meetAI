e952d5012f5530290cc6392cf46c8a286511821f
james-fsoft <57792460+james-fsoft@users.noreply.github.com>
2026-09-11 15:44:16 +0900

    Add hidden conference history route

---
+++ app/conference-history/page.tsx
@@ -0,0 +1,9 @@
+export default function ConferenceHistoryPage() {
+  return (
+    <iframe
+      src="/conference-history.html"
+      title="Flash Meet Conference History"
+      style={{ border: 0, width: "100%", height: "100vh", display: "block" }}
+    />
+  );
+}
