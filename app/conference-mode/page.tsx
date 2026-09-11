4e9252e8fc579087bbf3ece8a66f217c7a6b581e
james-fsoft <57792460+james-fsoft@users.noreply.github.com>
2026-09-11 15:43:52 +0900

    Upgrade hidden conference mode host

--- app/conference-mode/page.tsx
+++ app/conference-mode/page.tsx
@@ -1,9 +1,9 @@
 export default function ConferenceModePage() {
   return (
     <iframe
-      src="/conference-display.html"
+      src="/conference-pro.html"
       title="Flash Meet Conference Mode"
-      allow="microphone; clipboard-write"
+      allow="microphone; clipboard-write; fullscreen"
       style={{ border: 0, width: "100%", height: "100vh", display: "block" }}
     />
   );
